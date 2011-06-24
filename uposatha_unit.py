85bef8b0b12618ca2f2a51241979d5cd83d7d6db
Bhante Jhanarato <jhanarato@gmail.com>
2011-06-24 19:20:50 +0800

    converted unit test to use pyunit

--- uposatha_unit.py
+++ uposatha_unit.py
@@ -1,22 +1,25 @@
 import datetime
 import filecmp
+import unittest
+
 import uposatha
 from datetime import date
 
-startDate = datetime.date(2011, 07, 15)
-endDate = datetime.date(2011, 07, 23)
-weekNum = 1
+#startDate = datetime.date(2011, 07, 15)
+#endDate = datetime.date(2011, 07, 23)
+#weekNum = 1
 
 #uposatha.printWeek(weekNum, startDate, endDate)
 
-unit_good = "test_output.txt"
-unit_out = "unit_out/test_output.txt"
+class OriginalScriptTest(unittest.TestCase):
+    def testOriginalScript(self):
+        unit_good = "test_output.txt"
+        unit_out = "unit_out/test_output.txt"
 
-uposatha.originalScript(unit_out)
+        uposatha.originalScript(unit_out)
 
-if filecmp.cmp(unit_good, unit_out) == True:
-    print "unit test passed"
-else:
-    print "unit test failed"
+        self.assertTrue(filecmp.cmp(unit_good, unit_out))
 
+suite = unittest.TestLoader().loadTestsFromTestCase(OriginalScriptTest)
+unittest.TextTestRunner(verbosity=2).run(suite)            
     
