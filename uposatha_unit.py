78eb34986e2d44160b304e0e808e109a7fc5fba0
Bhante Jhanarato <jhanarato@gmail.com>
2011-06-24 20:07:42 +0800

    checks if unit test output exists and removes it in setUp()

--- uposatha_unit.py
+++ uposatha_unit.py
@@ -1,5 +1,6 @@
 import datetime
 import filecmp
+import os
 import unittest
 
 import uposatha
@@ -12,13 +13,19 @@ from datetime import date
 ##uposatha.printWeek(weekNum, startDate, endDate)
     
 class UposathaTest(unittest.TestCase):
+    unit_good = "test_output.txt"
+    unit_out = "unit_out/test_output.txt"
+    
+    def setUp(self):
+        if os.path.exists(self.unit_out):
+            os.remove(self.unit_out)
+        
     def testOriginalScript(self):
-        unit_good = "test_output.txt"
-        unit_out = "unit_out/test_output.txt"
+        
         uposathaInstance = uposatha.Uposatha()
-        uposathaInstance.originalScript(unit_out)
+        uposathaInstance.originalScript(self.unit_out)
 
-        self.assertTrue(filecmp.cmp(unit_good, unit_out))
+        self.assertTrue(filecmp.cmp(self.unit_good, self.unit_out))
 
 suite = unittest.TestLoader().loadTestsFromTestCase(UposathaTest)
 unittest.TextTestRunner(verbosity=2).run(suite)            
