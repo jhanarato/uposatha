52ea3bd740f4212f6430ae17eeb1ffac17cc79de
Bhante Jhanarato <jhanarato@gmail.com>
2011-06-24 19:34:17 +0800

    Created Uposatha class

--- uposatha.py
+++ uposatha.py
@@ -1,70 +1,70 @@
 import datetime
 from datetime import date, timedelta
 
-def printWeek(weekNum, startDate, endDate):
-    print "Week %02d: %s TO %s\n" % (weekNum, startDate.isoformat(), endDate.isoformat() + "\n")
+class Uposatha:
+    def printWeek(self, toFile, weekNum, startDate, endDate):
+        outFile.write("Week %02d: %s TO %s\n" % (weekNum, startDate.isoformat(), endDate.isoformat() + "\n"))
 
+    def originalScript(self, outputFile):  
+        dayBeforeRains = datetime.date(2011, 07, 15) # First day of rains, after uposatha
+        week1start = dayBeforeRains + timedelta(1)
+        week1end = dayBeforeRains + timedelta(8)
 
-def originalScript(outputFile):  
-    dayBeforeRains = datetime.date(2011, 07, 15) # First day of rains, after uposatha
-    week1start = dayBeforeRains + timedelta(1)
-    week1end = dayBeforeRains + timedelta(8)
+        out = open(outputFile, 'w')
+        
+        out.write("Week 01: " + week1start.isoformat() + " TO " + week1end.isoformat() + "\n")
 
-    out = open(outputFile, 'w')
-    
-    out.write("Week 01: " + week1start.isoformat() + " TO " + week1end.isoformat() + "\n")
+        week2start = week1end + timedelta(1)
+        week2end =   week1end + timedelta(7)
 
-    week2start = week1end + timedelta(1)
-    week2end =   week1end + timedelta(7)
+        out.write("Week 02: " + week2start.isoformat() + " TO " + week2end.isoformat() + "\n")
 
-    out.write("Week 02: " + week2start.isoformat() + " TO " + week2end.isoformat() + "\n")
+        week3start = week2end + timedelta(1)
+        week3end =   week2end + timedelta(8)
 
-    week3start = week2end + timedelta(1)
-    week3end =   week2end + timedelta(8)
+        out.write("Week 03: " + week3start.isoformat() + " TO " + week3end.isoformat() + "\n")
 
-    out.write("Week 03: " + week3start.isoformat() + " TO " + week3end.isoformat() + "\n")
+        week4start = week3end + timedelta(1)
+        week4end =   week3end + timedelta(7)
 
-    week4start = week3end + timedelta(1)
-    week4end =   week3end + timedelta(7)
+        out.write("Week 04: " + week4start.isoformat() + " TO " + week4end.isoformat() + "\n")
 
-    out.write("Week 04: " + week4start.isoformat() + " TO " + week4end.isoformat() + "\n")
+        week5start = week4end + timedelta(1)
+        week5end =   week4end + timedelta(7)
 
-    week5start = week4end + timedelta(1)
-    week5end =   week4end + timedelta(7)
+        out.write("Week 05: " + week5start.isoformat() + " TO " + week5end.isoformat() + "\n")
 
-    out.write("Week 05: " + week5start.isoformat() + " TO " + week5end.isoformat() + "\n")
+        week6start = week5end + timedelta(1)
+        week6end =   week5end + timedelta(7)
 
-    week6start = week5end + timedelta(1)
-    week6end =   week5end + timedelta(7)
+        out.write("Week 06: " + week6start.isoformat() + " TO " + week6end.isoformat() + "\n")
 
-    out.write("Week 06: " + week6start.isoformat() + " TO " + week6end.isoformat() + "\n")
+        week7start = week6end + timedelta(1)
+        week7end =   week6end + timedelta(8)
 
-    week7start = week6end + timedelta(1)
-    week7end =   week6end + timedelta(8)
+        out.write("Week 07: " + week7start.isoformat() + " TO " + week7end.isoformat() + "\n")
 
-    out.write("Week 07: " + week7start.isoformat() + " TO " + week7end.isoformat() + "\n")
+        week8start = week7end + timedelta(1)
+        week8end =   week7end + timedelta(7)
 
-    week8start = week7end + timedelta(1)
-    week8end =   week7end + timedelta(7)
+        out.write("Week 08: " + week8start.isoformat() + " TO " + week8end.isoformat() + "\n")
 
-    out.write("Week 08: " + week8start.isoformat() + " TO " + week8end.isoformat() + "\n")
+        week9start = week8end + timedelta(1)
+        week9end =   week8end + timedelta(8)
 
-    week9start = week8end + timedelta(1)
-    week9end =   week8end + timedelta(8)
+        out.write("Week 09: " + week9start.isoformat() + " TO " + week9end.isoformat() + "\n")
 
-    out.write("Week 09: " + week9start.isoformat() + " TO " + week9end.isoformat() + "\n")
+        week10start = week9end + timedelta(1)
+        week10end =   week9end + timedelta(7)
 
-    week10start = week9end + timedelta(1)
-    week10end =   week9end + timedelta(7)
+        out.write("Week 10: " + week10start.isoformat() + " TO " + week10end.isoformat() + "\n")
 
-    out.write("Week 10: " + week10start.isoformat() + " TO " + week10end.isoformat() + "\n")
+        week11start = week10end + timedelta(1)
+        week11end =   week10end + timedelta(8)
 
-    week11start = week10end + timedelta(1)
-    week11end =   week10end + timedelta(8)
+        out.write("Week 11: " + week11start.isoformat() + " TO " + week11end.isoformat() + "\n")
 
-    out.write("Week 11: " + week11start.isoformat() + " TO " + week11end.isoformat() + "\n")
+        week12start = week11end + timedelta(1)
+        week12end =   week11end + timedelta(7)
 
-    week12start = week11end + timedelta(1)
-    week12end =   week11end + timedelta(7)
-
-    out.write( "Week 12: " + week12start.isoformat() + " TO " + week12end.isoformat())
+        out.write( "Week 12: " + week12start.isoformat() + " TO " + week12end.isoformat())
--- uposatha_unit.py
+++ uposatha_unit.py
@@ -5,21 +5,21 @@ import unittest
 import uposatha
 from datetime import date
 
-#startDate = datetime.date(2011, 07, 15)
-#endDate = datetime.date(2011, 07, 23)
-#weekNum = 1
-
-#uposatha.printWeek(weekNum, startDate, endDate)
-
-class OriginalScriptTest(unittest.TestCase):
+##startDate = datetime.date(2011, 07, 15)
+##endDate = datetime.date(2011, 07, 23)
+##weekNum = 1
+##
+##uposatha.printWeek(weekNum, startDate, endDate)
+    
+class UposathaTest(unittest.TestCase):
     def testOriginalScript(self):
         unit_good = "test_output.txt"
         unit_out = "unit_out/test_output.txt"
-
-        uposatha.originalScript(unit_out)
+        uposathaInstance = uposatha.Uposatha()
+        uposathaInstance.originalScript(unit_out)
 
         self.assertTrue(filecmp.cmp(unit_good, unit_out))
 
-suite = unittest.TestLoader().loadTestsFromTestCase(OriginalScriptTest)
+suite = unittest.TestLoader().loadTestsFromTestCase(UposathaTest)
 unittest.TextTestRunner(verbosity=2).run(suite)            
     
