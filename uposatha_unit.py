6267347651ddfea00c33606d2222c9903b7c73ae
Bhante Jhanarato <jhanarato@gmail.com>
2011-06-24 19:00:03 +0800

    initial unit test added, passed

--- uposatha_unit.py
+++ uposatha_unit.py
@@ -1,4 +1,5 @@
 import datetime
+import filecmp
 import uposatha
 from datetime import date
 
@@ -6,4 +7,16 @@ startDate = datetime.date(2011, 07, 15)
 endDate = datetime.date(2011, 07, 23)
 weekNum = 1
 
-uposatha.printWeek(weekNum, startDate, endDate)
+#uposatha.printWeek(weekNum, startDate, endDate)
+
+unit_good = "test_output.txt"
+unit_out = "unit_out/test_output.txt"
+
+uposatha.originalScript(unit_out)
+
+if filecmp.cmp(unit_good, unit_out) == True:
+    print "unit test passed"
+else:
+    print "unit test failed"
+
+    
