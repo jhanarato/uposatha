2ebf336e2729c555366ce75f24a4a864b9d655ab
Bhante Jhanarato <jhanarato@gmail.com>
2011-06-24 18:26:18 +0800

    preparing unit tests, began extracting first method

---
+++ uposatha_unit.py
@@ -0,0 +1,9 @@
+import datetime
+import uposatha
+from datetime import date
+
+startDate = datetime.date(2011, 07, 15)
+endDate = datetime.date(2011, 07, 23)
+weekNum = 1
+
+uposatha.printWeek(weekNum, startDate, endDate)
