791c61ec54d3cb83631cfc075a5728e476314be2
Ryan O'Connell <rymocon@gmail.com>
2014-05-10 14:37:29 -0400

    Use constructor; remove file IO

--- uposatha.py
+++ uposatha.py
@@ -1,45 +1,38 @@
-import datetime
 from datetime import date, timedelta
 
 class Uposatha:
-    rainsWeeks = [8,7, 8,7, 7,7, 8,7, 8,7, 8,7, 7,7, 8,7]
-    weekNum = 0 # Current week index
+    def __init__(self, filename):
+        self.rainsWeeks = [8,7, 8,7, 7,7, 8,7, 8,7, 8,7, 7,7, 8,7]
+        self.weekNum = 0 # Current week index
+        
+        self.nextWeekStartDate = None # initialised by nextWeek()
+        self.nextWeekEndDate = date(2011, 07, 15) # Uposatha prior to season
     
-    nextWeekStartDate = None # initialised by nextWeek()
-    nextWeekEndDate = datetime.date(2011, 07, 15) # Uposatha prior to season
-
-    self.out = open(fileName, 'w')
-
-    # A simple formatting method
     def formatWeek(self):
         return "Week %02d: %s TO %s" % (self.weekNum,
                                         self.nextWeekStartDate.isoformat(),
                                         self.nextWeekEndDate.isoformat())
-        
+    
     def nextWeek(self):
         lastWeekEndDate = self.nextWeekEndDate
         self.nextWeekStartDate = lastWeekEndDate + timedelta(1)
         self.nextWeekEndDate   = lastWeekEndDate + timedelta(self.rainsWeeks[self.weekNum])
         self.weekNum = self.weekNum + 1
-
-    # The original script, now being refactored
+    
     def originalScript(self):
         for week in range(1, 12):
             self.nextWeek()
-            self.out.write(self.formatWeek() + "\n")
-
+            print(self.formatWeek() + "\n")
+        
         self.nextWeek()
-        self.out.write(self.formatWeek()) # Last line has no line feed
-        self.out.close()
+        print(self.formatWeek()) # Last line has no line feed
 
 class UposathaWriter:
-    # A simple formatting method
     def formatWeek(self, weekNum, startDate, endDate):
         return "Week %02d: %s TO %s" % (self.weekNum,
                                         startDate.isoformat(),
                                         endDate.isoformat())
 
 # Main
-u = Uposatha()
-u.setOutput("out.txt")
+u = Uposatha("out.txt")
 u.originalScript()
