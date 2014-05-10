e6fa52649c24052c57a8cec982ec8b6f30e044c6
Ryan O'Connell <rymocon@gmail.com>
2014-05-10 15:58:20 -0400

    Add comments; remove filename

--- uposatha.py
+++ uposatha.py
@@ -3,14 +3,20 @@
 from datetime import date, datetime, timedelta
 
 class Uposatha:
-    def __init__(self, filename, prevUpo):
+    def __init__(self, prevUpo):
+        """
+        Constructor
+        
+        prevUpo: the Uposatha prior to season. Begins as yyyy/mm/dd str,
+        converted to date.
+        
+        nextA, nextB = start/end days of next week
+        lastB = end day of last week
+        """
         self.rains = [8,7, 8,7, 7,7, 8,7, 8,7, 8,7, 7,7, 8,7]
         self.weekNo = 0 # Current week index
         
-        """prevUpo: the Uposatha prior to season
-        Begins as yyyy/mm/dd str, converted to date"""
-        prevUpo = datetime.strptime(prevUpo, "%Y/%m/%d").date()
-        self.nextB = prevUpo
+        self.nextB = datetime.strptime(prevUpo, "%Y/%m/%d").date()
         self.nextA = None # initialised by advance()
     
     def calendar(self):
@@ -30,10 +36,12 @@ class Uposatha:
     
     def _advance(self):
         lastB = self.nextB
+        # next week's start is the day after last week's end
         self.nextA = lastB + timedelta(1)
+        # next week's end depends on the pattern of days in self.rains
         self.nextB = lastB + timedelta(self.rains[self.weekNo])
         self.weekNo += 1
 
 if __name__ == "__main__":
-    u = Uposatha("out.txt", "2014/7/11")
+    u = Uposatha("2014/7/11")
     u.calendar()
