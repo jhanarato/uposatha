6a61c381a669b81890a958f142717226efb9eb53
Ryan O'Connell <rymocon@gmail.com>
2014-05-10 15:53:26 -0400

    Reordering/renaming; make prevUpo a constructor arg

--- uposatha.py
+++ uposatha.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
 
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 
 class Uposatha:
-    def __init__(self, filename):
-        self.rainsWeeks = [8,7, 8,7, 7,7, 8,7, 8,7, 8,7, 7,7, 8,7]
+    def __init__(self, filename, prevUpo):
+        self.rains = [8,7, 8,7, 7,7, 8,7, 8,7, 8,7, 7,7, 8,7]
         self.weekNo = 0 # Current week index
+        
+        """prevUpo: the Uposatha prior to season
+        Begins as yyyy/mm/dd str, converted to date"""
+        prevUpo = datetime.strptime(prevUpo, "%Y/%m/%d").date()
+        self.nextB = prevUpo
         self.nextA = None # initialised by advance()
-        self.nextB = date(2011, 07, 15) # Uposatha prior to season
     
-    def getWeek(self):
+    def calendar(self):
+        """Print starting and ending date for week ending in Uposatha"""
+        for i in range(1, 12):
+            self._advance()
+            print(self._getWeek())
+        self._advance()
+        print(self._getWeek()) # Last line has no line feed
+    
+    def _getWeek(self):
         return "Week {no:02d}: {start} -> {end}".format(
             no=self.weekNo,
             start=self.nextA.isoformat(),
             end=self.nextB.isoformat()
         )
     
-    def advance(self):
+    def _advance(self):
         lastB = self.nextB
         self.nextA = lastB + timedelta(1)
-        self.nextB = lastB + timedelta(self.rainsWeeks[self.weekNo])
+        self.nextB = lastB + timedelta(self.rains[self.weekNo])
         self.weekNo += 1
-    
-    def calendar(self):
-        for i in range(1, 12):
-            self.advance()
-            print(self.getWeek())
-        self.advance()
-        print(self.getWeek()) # Last line has no line feed
 
-# Main
-u = Uposatha("out.txt")
-u.calendar()
+if __name__ == "__main__":
+    u = Uposatha("out.txt", "2014/7/11")
+    u.calendar()
