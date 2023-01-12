1d3d2177cc2fdd15ef3ac306cc7aa5d1a0f7eef0
Ajahn Jhanarato <jhanarato@gmail.com>
2023-01-12 10:25:39 +0800

    Added details to configure.py

--- src/uposatha/configure.py
+++ src/uposatha/configure.py
@@ -1,1 +1,10 @@
 # Configure the calendar model.
+
+import datetime
+
+HOT_SEASON = "Gimha"
+
+date_before_first_season = datetime.date(2010, 2, 28)
+first_season = HOT_SEASON
+years_with_long_hot_season = [2010, 2012, 2015, 2018, 2021, 2023, 2026, 2029]
+years_with_extra_days = [2016, 2020, 2025, 2030]
