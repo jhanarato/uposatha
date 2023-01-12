067b0367f8c88fd38f149fd9e5fba131c9afb4da
Ajahn Jhanarato <jhanarato@gmail.com>
2023-01-12 10:25:04 +0800

    Moving stuff around, creating module structure.

--- src/uposatha/assemble.py
+++ src/uposatha/assemble.py
@@ -1,24 +1,2 @@
-class UposathaSequence:
-    def __init__(self):
-        self.add_month = False
-        self.add_day = False
-        self.positions = 8
-        self.fourteen_days = [2, 6]
-        self.extra_day_position = 6
+from uposatha.sequence import UposathaSequence
 
-    def _duration_at_position(self, position):
-        if position in self.fourteen_days:
-            return 14
-        else:
-            return 15
-
-    @property
-    def days(self):
-        if self.add_month:
-            self.positions += 2
-
-        if self.add_day:
-            self.fourteen_days.remove(6)
-
-        positions = range(self.positions)
-        return [self._duration_at_position(position) for position in positions]
