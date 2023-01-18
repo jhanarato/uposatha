b810a880b3b797278538aed3b67afa2c7d706f27
Ajahn Jhanarato <jhanarato@gmail.com>
2023-01-18 16:34:16 +0800

    Refactored uposatha sequences to use dictionary lookup.

--- src/uposatha/sequence.py
+++ src/uposatha/sequence.py
@@ -1,6 +1,18 @@
 from typing import List
 from uposatha.elements import SeasonName, SeasonType
 
+days_between_uposathas = {
+    SeasonType.NORMAL: [15, 15, 14, 15, 15, 15, 14, 15],
+    SeasonType.EXTRA_MONTH: [15, 15, 14, 15, 15, 15, 14, 15, 15, 15],
+    SeasonType.EXTRA_DAY: [15, 15, 14, 15, 15, 15, 15, 15]
+}
+
+days_between_half_moons = {
+    SeasonType.NORMAL: [8, 15, 15, 14, 15, 15, 15, 14],
+    SeasonType.EXTRA_MONTH: [8, 15, 15, 14, 15, 15, 15, 14, 15, 15],
+    SeasonType.EXTRA_DAY: [8, 15, 15, 14, 15, 15, 15, 15]
+}
+
 normal_uposatha_sequence = [15, 15, 14, 15, 15, 15, 14, 15]
 extra_month_uposatha_sequence = [15, 15, 14, 15, 15, 15, 14, 15, 15, 15]
 extra_day_uposatha_sequence = [15, 15, 14, 15, 15, 15, 15, 15]
@@ -15,13 +27,8 @@ class SequenceSelector:
         self._extra_day_years = extra_day_years
 
     def uposathas(self, season_name: SeasonName, begins_in_year: int) -> List[int]:
-        if self._season_type(season_name, begins_in_year) == SeasonType.EXTRA_MONTH:
-            return extra_month_uposatha_sequence
-
-        if self._season_type(season_name, begins_in_year) == SeasonType.EXTRA_DAY:
-            return extra_day_uposatha_sequence
-
-        return normal_uposatha_sequence
+        season_type = self._season_type(season_name, begins_in_year)
+        return days_between_uposathas[season_type]
 
     def half_moons(self, season_name: SeasonName, begins_in_year: int) -> List[int]:
         if self._season_type(season_name, begins_in_year) == SeasonType.EXTRA_MONTH:
