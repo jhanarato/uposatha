8618ab64d457988ecb14c4d25fc51a18fc818e48
Ajahn Jhanarato <jhanarato@gmail.com>
2023-01-18 08:55:56 +0800

    Renamed fixtures.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 
 @pytest.fixture
-def normal_sequence():
+def normal_uposatha_sequence():
     return [15, 15, 14, 15, 15, 15, 14, 15]
 
 @pytest.fixture
-def long_hot_season_sequence():
+def extra_month_uposatha_sequence():
     return [15, 15, 14, 15, 15, 15, 14, 15, 15, 15]
 
 @pytest.fixture
-def extra_day_sequence():
+def extra_day_uposatha_sequence():
     return [15, 15, 14, 15, 15, 15, 15, 15]
