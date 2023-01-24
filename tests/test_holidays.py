4146e800d73cdbf5af2abde3507f3875c7dcdbc2
Ajahn Jhanarato <jhanarato@gmail.com>
2023-01-24 16:49:10 +0800

    Getting ready to test holidays.

--- tests/test_holidays.py
+++ tests/test_holidays.py
@@ -1,6 +1,11 @@
+from datetime import date
+
 import pytest
 
 from uposatha.elements import HolidayName
 
-def test_pavarana_day(rainy_season):
+def test_pavarana_day_name(rainy_season):
     assert rainy_season.holidays[0].name == HolidayName.PAVARANA
+
+# def test_pavarana_day_date(rainy_season):
+#     assert rainy_season.holidays[0].falls_on == date(2010, 10, 23)
