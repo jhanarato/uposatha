6f9ea70582672a55c87aaf9d3ee3268228e8441c
Ajahn Jhanarato <jhanarato@gmail.com>
2023-01-30 10:49:01 +0800

    Added test for magha puja.

--- tests/test_holidays.py
+++ tests/test_holidays.py
@@ -2,7 +2,7 @@ from datetime import date
 
 import pytest
 
-from uposatha.elements import HolidayName
+from uposatha.elements import HolidayName, SeasonName
 
 def test_pavarana_day_name(rainy_season):
     assert rainy_season.holidays[0].name == HolidayName.PAVARANA
@@ -10,5 +10,9 @@ def test_pavarana_day_name(rainy_season):
 def test_pavarana_day_date(rainy_season):
     assert rainy_season.holidays[0].uposatha.falls_on == date(2010, 10, 23)
 
-def test_magha_puja(cold_season):
-    pass
+def test_magha_puja_when_next_season_long_with_extra_month(cold_before_extra_month_season):
+    holidays = cold_before_extra_month_season.holidays
+    magha_pujas = [holiday for holiday in holidays if holiday.name == HolidayName.MAGHA_PUJA]
+    assert len(magha_pujas) == 1
+    assert magha_pujas[0].uposatha.number_in_season == 8
+    assert magha_pujas[0].uposatha.falls_on == date(2012, 3, 7)
